5f6f8cc9ab13e0c36ca3171815b9e5648edbae5b
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-15 15:20:44 -0400

    add mongo and User schema

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require('./services/passport');
+require('./models/User');
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
 authRoutes(app);
+
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
