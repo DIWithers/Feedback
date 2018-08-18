1ce70adc383cd9013b563f157a21cb06c121229f
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-17 20:46:05 -0400

    can log out with redirect using full http request

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,11 +5,11 @@ class Header extends Component {
     renderContent() {
         switch(this.props.auth) {
             case null:
-                return "STILL DECIDING"
+                return;
             case false:
-                return (<li><a> Login With Google</a></li>)
+                return <li><a href="/auth/google"> Login With Google</a></li>;
             default:
-            return "LOGGED IN"
+            return <li><a href="/api/logout">LOGOUT</a>></li>
         }
     }
     render() {
