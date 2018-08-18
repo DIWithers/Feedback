2c6b10d760199daa3598650aaa84ed390607f1f0
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-18 14:39:47 -0400

    simple landing page styling

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{textAlign:'center'}}>
+            <h1>Emaily</h1>
+            <h4>Collect Feedback from your users</h4>
+        </div>
+    );
+};
+export default Landing;
