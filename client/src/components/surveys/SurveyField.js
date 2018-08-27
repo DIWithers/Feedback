6be2c4414021ccf523e9cc7a4ef54065e0e6ae0f
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-26 20:42:54 -0400

    validate required title; validation styling

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-export default ({ input, label }) => { //E6 extraction of input from 'props'
+export default ({ input, label, meta: { error, touched } }) => {
     return (
         <div>
             <label>{label}</label>
-            <input {...input} />
+            <input {...input} style={{ marginBottom: '5px' }} />
+            <div className="red-text" style={{ marginBottom: '20px' }}>
+                {touched && error}
+            </div>
         </div>
     );
 };
