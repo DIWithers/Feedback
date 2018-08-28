dd45f698384a951e3b01e9c5b68fcded833dda11
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-28 17:46:57 -0400

    can now submit survey

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
     const reviewFields = formFields.map( ({ name, label }) => {
         return (
             <div key={name}>
@@ -16,10 +17,17 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
             <h5>Please confirm your entries.</h5>
             { reviewFields }
             <button
-                className="yellow darken-3 btn-flat"
+                className="yellow darken-3 white-text btn-flat"
                 onClick={ onCancel }
             >
             Back
+            <i className="material-icons medium left">arrow_back</i>
+            </button>
+            <button
+                onClick={ () => submitSurvey(formValues) }
+                className="green white-text btn-flat right">
+                Send Survey
+                <i className="material-icons right">email</i>
             </button>
         </div>
     );
@@ -29,4 +37,4 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -1,6 +1,6 @@
 export default [
     {label: "Survey Title", type: "text", name: "title", required: true},
-    {label: "Survey Line", type: "text", name: "subject", required: true},
+    {label: "Subject Line", type: "text", name: "subject", required: true},
     {label: "Email Body", type: "text", name: "body", required: true},
     {label: "Recipient List", name: "recipients", required: true}
 ];
