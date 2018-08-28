d2b48f9bf84a65d3b8b535b7aa86728da262b6b4
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-27 20:54:02 -0400

    match front end property names to backend property names

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
     {label: "Survey Title", type: "text", name: "title", required: true},
     {label: "Survey Line", type: "text", name: "subject", required: true},
     {label: "Email Body", type: "text", name: "body", required: true},
-    {label: "Recipient List", name: "emails", required: true}
+    {label: "Recipient List", name: "recipients", required: true}
 ];
