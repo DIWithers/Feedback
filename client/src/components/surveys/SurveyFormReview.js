9c9921154b362e53acfbb653f88f93d90bd42e4c
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-28 19:27:11 -0400

    redirect to dashboard after submitting a survey while making use of 'withRouter'

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import formFields from './formFields';
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = formFields.map( ({ name, label }) => {
         return (
             <div key={name}>
@@ -24,7 +25,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
             <i className="material-icons medium left">arrow_back</i>
             </button>
             <button
-                onClick={ () => submitSurvey(formValues) }
+                onClick={ () => submitSurvey(formValues, history) }
                 className="green white-text btn-flat right">
                 Send Survey
                 <i className="material-icons right">email</i>
@@ -37,4 +38,4 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
