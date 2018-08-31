dfc584fdf1b816b1086fbebb5b9ad69a0ae8e806
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-31 15:01:48 -0400

    update database with first time respondents' choice

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -38,28 +38,43 @@ module.exports = app => {
     });
     app.post('/api/surveys/webhooks', (req, res) => {
         const pathVariableExtractor = new Path('/api/surveys/:surveyId/:choice');
-        const uniqueEvents = req.body
-            .filter(event => event.email && event.url && event.event === 'click')
-            .map( ({ email, url }) => {
-                const match = pathVariableExtractor.test(new URL(url).pathname);
-                if (match) return { email, surveyId: match.surveyId, choice: match.choice };
-                })
-            .sort((e1, e2) => {
-                const e1_sort_string = e1.email + e1.surveyId + e1.choice;
-                const e2_sort_string = e2.email + e2.surveyId + e2.choice;
-            
-                if (e1_sort_string < e2_sort_string) {
-                return -1
-                }
-                return 1;
-            })
-            .reduce((acc, curr) => {
-                if (acc.length === 0 || acc[acc.length-1] !== curr) {
-                  acc.push(curr);
-                }
-                return acc;       
-              }, []);
+        updateDBWithSelectedChoiceForFirstTimeRespondents(req, pathVariableExtractor);
         res.send({});
             // Can I use ES6 Set here?
     });
 };
+
+function updateDBWithSelectedChoiceForFirstTimeRespondents(req, pathVariableExtractor) {
+    req.body
+        .filter(event => event.email && event.url && event.event === 'click')
+        .map(({ email, url }) => {
+            const match = pathVariableExtractor.test(new URL(url).pathname);
+            if (match)
+                return { email, surveyId: match.surveyId, choice: match.choice };
+        })
+        .sort((e1, e2) => {
+            const e1_sort_string = e1.email + e1.surveyId + e1.choice;
+            const e2_sort_string = e2.email + e2.surveyId + e2.choice;
+            if (e1_sort_string < e2_sort_string) {
+                return -1;
+            }
+            return 1;
+        })
+        .reduce((acc, curr) => {
+            if (acc.length === 0 || acc[acc.length - 1] !== curr) {
+                acc.push(curr);
+            }
+            return acc;
+        }, [])
+        .array.forEach(({ surveyId, email, choice }) => {
+            Survey.updateOne({
+                _id: surveyId,
+                recipients: {
+                    $elemMatch: { email: email, responded: false }
+                }
+            }, {
+                    $inc: { [choice]: 1 },
+                    $set: { 'recipients.$.responded': true }
+            }).exec();
+        })
+}
