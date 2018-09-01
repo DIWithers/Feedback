f6cce939da1ac3d228b2b9656360b8577a23fff7
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-01 15:39:35 -0400

    heroku debugging

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -50,44 +50,44 @@ module.exports = app => {
     });
 };
 
-function updateDBWithSelectedChoiceForFirstTimeRespondents(req, pathVariableExtractor) {
-    getUniqueEvents(req, pathVariableExtractor)
-        .forEach(({ surveyId, email, choice }) => {
-            Survey.updateOne(
-                {
-                    _id: surveyId,
-                    recipients: {
-                        $elemMatch: { email: email, responded: false }
-                    }
-                },
-                {
-                    $inc: { [choice]: 1 },
-                    $set: { 'recipients.$.responded': true },
-                    lastResponded: new Date()
-                }
-            ).exec();
-        })
-}
-function getUniqueEvents(req, pathVariableExtractor) {
-    return req.body
-        .filter(event => event.email && event.url && event.event === 'click')
-        .map(({ email, url }) => {
-            const match = pathVariableExtractor.test(new URL(url).pathname);
-            if (match)
-                return { email, surveyId: match.surveyId, choice: match.choice };
-        })
-        .sort((e1, e2) => {
-            const e1_sort_string = e1.email + e1.surveyId + e1.choice;
-            const e2_sort_string = e2.email + e2.surveyId + e2.choice;
-            if (e1_sort_string < e2_sort_string) {
-                return -1;
-            }
-            return 1;
-        })
-        .reduce((acc, curr) => {
-            if (acc.length === 0 || acc[acc.length - 1] !== curr) {
-                acc.push(curr);
-            }
-            return acc;
-        }, []);
-}
+// function updateDBWithSelectedChoiceForFirstTimeRespondents(req, pathVariableExtractor) {
+//     getUniqueEvents(req, pathVariableExtractor)
+//         .forEach(({ surveyId, email, choice }) => {
+//             Survey.updateOne(
+//                 {
+//                     _id: surveyId,
+//                     recipients: {
+//                         $elemMatch: { email: email, responded: false }
+//                     }
+//                 },
+//                 {
+//                     $inc: { [choice]: 1 },
+//                     $set: { 'recipients.$.responded': true },
+//                     lastResponded: new Date()
+//                 }
+//             ).exec();
+//         })
+// }
+// function getUniqueEvents(req, pathVariableExtractor) {
+//     return req.body
+//         .filter(event => event.email && event.url && event.event === 'click')
+//         .map(({ email, url }) => {
+//             const match = pathVariableExtractor.test(new URL(url).pathname);
+//             if (match)
+//                 return { email, surveyId: match.surveyId, choice: match.choice };
+//         })
+//         .sort((e1, e2) => {
+//             const e1_sort_string = e1.email + e1.surveyId + e1.choice;
+//             const e2_sort_string = e2.email + e2.surveyId + e2.choice;
+//             if (e1_sort_string < e2_sort_string) {
+//                 return -1;
+//             }
+//             return 1;
+//         })
+//         .reduce((acc, curr) => {
+//             if (acc.length === 0 || acc[acc.length - 1] !== curr) {
+//                 acc.push(curr);
+//             }
+//             return acc;
+//         }, []);
+// }
